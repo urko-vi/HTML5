e0414e2a97c4482ce42e5405d695da248ff91e7c
urko villanueva <urko.informatica@gmail.com>
2016-12-01 18:01:21 +0100

    Notas javaScript

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
 $.noConflict();
+var nombres = ["imanol"];
+var apellidos = ["jimenez lopez"];
+var nUF1841 = new Array();
+nUF1841['imanol'] = 5;
+var nUF1842 = [5];
+var nUF1843 = [5];
+var nUF1844 = [5];
+var nUF1845 = [5];
+var nUF1846 = [5];
 
 jQuery(document).ready(function($) {
+    function cargarAlumnos() {
+        for (var i = 0; i < nombres.length; i++) {
+            var nombre = nombres[i];
+            var apellido = apellidos[i];
+            var html_text = "<tr>" +
+                "<td><input type='checkbox' value=''/> </td>" +
+                "<td>" + nombre + "</td>" +
+                "<td>" + apellido + "</td>" +
+                "<td>" + nUF1841[nombre] + "</td>" +
+                "<td></td>" +
+                "<td></td>" +
+                "<td></td>" +
+                "<td></td>" +
+                "<td></td>" +
+                "<td></td>" +
+                "<td><button>Editar</button></td>" +
+                "</tr>";
+            $('#listado-alumnos tbody').append(html_text);
+        }
+    }
+
+    cargarAlumnos();
     /*
     function tracear(){
         // boolean, numericas, texto, Array (Object)
